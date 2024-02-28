693d047027e23257bbea7937152258d4ef0238fd
daxesh2002 <daxesh.tadvi@bcssarl.com>
2024-02-28 15:38:31 +0530

    database add

--- view/src/App.js
+++ view/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import 'semantic-ui-css/semantic.min.css'
 function App() {
   return (
     <div className="App">
