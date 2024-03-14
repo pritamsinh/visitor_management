37e50fe0311c7b3c5ac04d6b71d34bdc6a13699e
Pritam Parmar <pritam.parmar@bcssarl.com>
2024-03-14 17:39:57 +0530

    cros issue fix

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const port = 3000;
 
 const server = http.createServer(app);
-
+console.log("server file")
 server.listen(port);
--- view/src/main.jsx
+++ view/src/main.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from "axios";
 
+axios.defaults.withCredentials = true; 
+axios.defaults.headers = {
+  'Cache-Control': 'no-cache',
+  'Pragma': 'no-cache',
+  'Expires': '0',
+  'Access-Control-Allow-Origin' : "*"
+};
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
